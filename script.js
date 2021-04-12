c41b9d04c720c93232a9e62d1e71de645a3ea0fa
American-college1881 <82362553+American-college1881@users.noreply.github.com>
2021-04-12 14:50:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let button  = document.querySelector(".button")
+let card  = document.querySelector(".credit-card")
+let main = document.querySelector(".main")
+
+button.addEventListener("click",function(){
+
+    card.style.display = "block"
+    main.style.height = "1470px"
+
+})
